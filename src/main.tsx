c10afc50cca9cb07447b6b89a0c1341678ac810e
MariaMoraisRenovaBE <maria.morais@renovabe.com.br>
2026-01-11 14:24:17 -0300

    Importar globals.css no main.tsx para aplicar estilos do Hero

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './styles/globals.css';
 
 // Renderiza o React imediatamente
 createRoot(document.getElementById('root')!).render(
